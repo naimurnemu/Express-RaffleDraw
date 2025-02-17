929e345b2365aec4bf0fada5c29089bb609ff218
naimurnemu <naimurrahaman82@gmail.com>
2025-02-17 21:05:08 +0600

    create ticket model

---
+++ src/Ticket.js
@@ -0,0 +1,19 @@
+const shortid = require("shortid");
+
+/**
+ * Ticket constractor holds ticket details
+ * @param {string} username - username of ticket owner
+ * @param {number} price - price of the ticket
+ * @instance Ticket object
+ */
+class Ticket {
+  constructor(username, price) {
+    this.ticket_id = shortid.generate();
+    this.username = username;
+    this.price = price;
+    this.created_at = new Date();
+    this.updated_at = new Date();
+  }
+}
+
+module.exports = Ticket;
